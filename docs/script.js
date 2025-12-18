083e266cff5c873db2d53afc787916424d422802
hudadat34 <hudadat27@gmail.com>
2025-12-18 15:48:37 +0300

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("contactForm");
+const messageBox = document.getElementById("formMessage");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  if (name === "" || email === "" || message === "") {
+    messageBox.style.color = "red";
+    messageBox.textContent = "Lütfen tüm alanları doldurun.";
+    return;
+  }
+
+  if (!email.includes("@")) {
+    messageBox.style.color = "red";
+    messageBox.textContent = "Geçerli bir e-posta giriniz.";
+    return;
+  }
+
+  messageBox.style.color = "green";
+  messageBox.textContent = "Mesajınız başarıyla gönderildi!";
+
+  form.reset();
+});
+
